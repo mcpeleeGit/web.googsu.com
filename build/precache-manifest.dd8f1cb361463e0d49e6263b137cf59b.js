f0205850f2a59726418edb31094beebc684726f0
mcpeleeGit <mcpelee@gmail.com>
2020-05-25 23:31:49 +0900

    first setting

---
+++ build/precache-manifest.dd8f1cb361463e0d49e6263b137cf59b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad7972d9472a4167b9009208b41a76d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87512ca726f29cfe7b0e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85f839828c6b9bce6a0b",
+    "url": "/static/js/2.dc95dd21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc95dd21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87512ca726f29cfe7b0e",
+    "url": "/static/js/main.f92f2129.chunk.js"
+  },
+  {
+    "revision": "70ff50799aa99bc32488",
+    "url": "/static/js/runtime-main.4e48b45a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
