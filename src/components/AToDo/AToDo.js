ca05a92a77bdcdc7cf979c1d962237315b30cced
mcpeleeGit <mcpelee@gmail.com>
2020-08-12 01:37:45 +0900

    component

--- src/components/AToDo/AToDo.js
+++ src/components/AToDo/AToDo.js
@@ -3,11 +3,22 @@ import AToDoService from './AToDoService';
 
 export class AToDo extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            statetest : 'statetest'
+        }
+    }
+
     render(){
         return (
             <div>
                 <h1>AToDo</h1>
-                <AToDoService/>
+                <AToDoService name="parameter name" anotherdefault={3}/>
+                <p/>[{this.state.statetest}]
+                <button onClick={()=>
+                    this.setState({ statetest : this.state.statetest + 'statetest'})
+                }>teststate</button>
             </div>
         );
     }
--- src/components/AToDo/AToDoService.js
+++ src/components/AToDo/AToDoService.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AToDoService extends Component {
+    static defaultProps = {
+        anotherdefault : 'anotherdefault'
+    }
+    static propTypes = {
+        anotherdefault : PropTypes.string
+    }
     render() {
         return (
             <div>
-                AToDoService
+                AToDoService [{this.props.name}]
+                <p/>[[{this.props.namedefault}]]
+                <p/>[[{this.props.anotherdefault}]]
             </div>
         );
     }
 }
-
+AToDoService.defaultProps = {
+    namedefault: 'namedefault'
+}
 export default AToDoService;
