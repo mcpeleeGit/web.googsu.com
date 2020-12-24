37b1f2c3b4a6eb253f0a3a7309eb095a5800ea2c
mcpeleeGit <mcpelee@gmail.com>
2020-12-24 17:30:00 +0900

    upload

--- api/kakao/controller/loginController.php
+++ api/kakao/controller/loginController.php
@@ -1,8 +1,15 @@
 <?php
+require('api/kakao/service/KakaoAPIService.php');
 require('api/kakao/service/LoginService.php');
 require('api/kakao/dto/LoginCallBackRequestDTO.php');
 
 class loginController {
+
+    public function loginLink(){
+        $KakaoAPIService = new KakaoAPIService();
+        header( "Content-Type:application/json;charset=UTF-8" );
+        echo json_encode(array('result_code'=>200, 'result'=>$KakaoAPIService->getKakaoLoginLink()));               
+    }
 
     public function defaultMethod(){
         $LoginService = new LoginService();
