4704f2a1fc6b352a7641c7ba9b07f11e96ac5d60
mcpeleeGit <mcpelee@gmail.com>
2021-01-14 12:28:24 +0900

    upload

--- api/kakao/service/ChannelService.php
+++ api/kakao/service/ChannelService.php
@@ -4,19 +4,24 @@ class ChannelService extends service {
 
         //{"event":"added","id":"1111","id_type":"app_user_id","plus_friend_public_id":"_FLX","plus_friend_uuid":"@ad","updated_at":"2020-01-01T00:00:00Z"}'
 
-        $this->hasAccess($_POST["event"], $_POST["id"], $_POST["id_type"], $_POST["plus_friend_public_id"], $_POST["plus_friend_uuid"], $_POST["updated_at"]);
+        // Takes raw data from the request
+        $json = file_get_contents('php://input');
+
+        // Converts it into a PHP object
+        $data = json_decode($json);        
+
+        $this->hasAccess($data);
         Response::jsonReturn('excute', 'error'); 
     }
 
-    public function hasAccess($event, $id, $id_type, $plus_friend_public_id, $plus_friend_uuid, $updated_at){
+    public function hasAccess($data){
         //Write action to txt log
-        $log  = "/event: ".$event.
-                "/id: ".$id.
-                "/id_type: ".$id_type.
-                "/plus_friend_public_id: ".$plus_friend_public_id.
-                "/plus_friend_uuid: ".$plus_friend_uuid.
-                "/updated_at: ".$updated_at.PHP_EOL;
+        $log  = "/event: ".$data->event.
+                "/id: ".$data->id.
+                "/id_type: ".$data->id_type.
+                "/plus_friend_public_id: ".$data->plus_friend_public_id.
+                "/plus_friend_uuid: ".$data->plus_friend_uuid.
+                "/updated_at: ".$data->updated_at.PHP_EOL;
         file_put_contents('./channel_callback_log_'.date("j.n.Y").'.txt', $log, FILE_APPEND);
     }       
 }
-?>
