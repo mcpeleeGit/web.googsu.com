b7a80dc1d2c547cbf39a2464c191f37d004d32ef
mcpeleeGit <mcpelee@gmail.com>
2021-01-14 11:16:44 +0900

    upload

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-echo urldecode("c09n00j7QD28sy34_beKdo:APA91bEYH4Suf95Lhkegyk94pvPg7V11Q9if4XCpwUTThqkA3WlmVFoVsxg1lMD4aN84Tf7EoCPzbCXbtfjqDJ8_9e5wGYZZGn13lHN7mMGjcgMqlpN_fTbSWGTmgTzbsaJ2zVh07k3v");
-
-//require(__DIR__ . '/api/common/route.php');
-//Route::init($_SERVER['REQUEST_URI']);
+require(__DIR__ . '/api/common/route.php');
+Route::init($_SERVER['REQUEST_URI']);
 ?>
